a81238f000c9f2599e42379b45ea54fd4e978b58
Furkan <furkanerdgn99@gmail.com>
2023-09-02 01:29:24 +0300

    for clone latest

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useRef } from 'react';
 import { useContext } from 'react';
 import { ActivityContext } from '.././context/index.jsx';
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text,Flex, Button } from "@chakra-ui/react";
 import * as d3 from 'd3';
-import { ArrowDownIcon } from '@chakra-ui/icons';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 
 function createPieChart(containerRef, data) {
   d3.select(containerRef.current).selectAll("*").remove();
    // Set your desired stroke width here
   const height = 500;
   const width = 500;
-  const customSVGPath = "M 44.7293 91.6519 C 42.7565 94.4826 38.5732 94.5046 36.5617 91.6948 L 2.48476 44.0928 C -0.56352 39.8347 4.14297 34.3568 8.81008 36.7308 L 38.8665 52.0193 C 40.3069 52.7519 42.0113 52.7444 43.4427 51.9989 L 71.6663 37.3012 C 76.3505 34.8619 81.1335 40.4161 78.0245 44.6846 L 65.0052 62.5592 L 44.7293 91.6519 Z";
 
   const minDimension = Math.min(width, height);
   const radius = minDimension / 2 ; // Adjust for stroke width
@@ -18,23 +17,32 @@ function createPieChart(containerRef, data) {
   const svg = d3.select(containerRef.current).append("svg")
     .attr("width", "100%")
     .attr("height", "100%")
-    .attr("viewBox", `0 0 ${minDimension} ${minDimension}`)
+    .attr('viewBox', `0 0 ${minDimension} ${minDimension}`)
     .append("g")
     .attr("transform", `translate(${minDimension / 2},${minDimension / 2})`);
 
   const color = d3.scaleOrdinal(d3.schemeAccent);
 
   const pie = d3.pie()
-    .value(function (d) { return d.desire });
+    .value(function (d) { return d.desire })
 
   const data_ready = pie(data);
+  const data_labels = pie(data);
 
-  svg.append("path")
-  .attr("d", customSVGPath)
-  .attr("fill", "yellow")
-  .attr("transform", `translate(-${minDimension / 12},-${minDimension / 1.4})`);
-  
-  
+
+  var arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(radius)
+
+  svg
+  .selectAll('mySlices')
+  .data(data_ready)
+  .enter()
+  .append('text')
+  .text(function(d){ return d.activities })
+  .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+  .style("text-anchor", "middle")
+  .style("font-size", 17)
 
   svg
     .selectAll('whatever')
@@ -62,11 +70,14 @@ function Wheel() {
   return (activities.length === 0) ? (<Text>Add some activity</Text>) :
     (
       <>
-        <Box width="100%" p={2} textAlign="center" alignSelf="flex-end" >
-          <Heading >Wheel</Heading>
-        </Box>
-        <Box width="70%" ref={chartRef} height="70%" >
-        </Box>
+      <Flex width="100%" direction="column" gap={5} justifyContent="center" alignItems="center">
+        <ChevronDownIcon  color="pink.500" w={10} h={10} />
+        <Box width={["90%","50%"]}  ref={chartRef} height={["50%","50%"]}   />
+        <Button alignSelf="center" colorScheme="pink" variant="solid" size="lg" mt={2} mb={2} >
+        spin the wheel
+        </Button>
+      </Flex>
+
       </>
     );
 }
